734159333b684b490b6a245a1be7ffeb866c3b11
anish59 <anish.vahora5@gmail.com>
2017-04-19 19:59:58 +0530

    work going on in CameraFragment

--- app/src/main/java/com/showcase/fragments/CameraFragment.java
+++ app/src/main/java/com/showcase/fragments/CameraFragment.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,167 +31,203 @@ import java.util.ArrayList;
 
 public class CameraFragment extends Fragment {
 
-	private TextView emptyView;
-	private GridView mView;
-	private Context mContext;
+    private TextView emptyView;
+    private GridView mView;
+    private Context mContext;
 
 
-	public static ArrayList<PhoneMediaControl.PhotoEntry> photos = new ArrayList<PhoneMediaControl.PhotoEntry>();
-	public static ArrayList<PhoneMediaControl.AlbumEntry> albumsSorted = null;
+    public static ArrayList<PhoneMediaControl.PhotoEntry> photos = new ArrayList<PhoneMediaControl.PhotoEntry>();
+    public static ArrayList<PhoneMediaControl.AlbumEntry> albumsSorted = null;
 
-	private Integer cameraAlbumId = null;
-	private PhoneMediaControl.AlbumEntry selectedAlbum = null;
-	private int itemWidth = 100;
-	private CameraAdapter cameraAdapter;
+    private Integer cameraAlbumId = null;
+    private PhoneMediaControl.AlbumEntry selectedAlbum = null;
+    private int itemWidth = 100;
+    private ListAdapter listAdapter;
 
 
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		/** Inflating the layout for this fragment **/
-		mContext = this.getActivity();
-		View v = inflater.inflate(R.layout.fragment_gallery, null);
-		initializeView(v);
-		return v;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Inflating the layout for this fragment **/
+        mContext = this.getActivity();
+        View v = inflater.inflate(R.layout.fragment_gallery, null);
+        initializeView(v);
+        return v;
+    }
 
-	private void initializeView(View v){
-		mView=(GridView)v.findViewById(R.id.grid_view);
-		emptyView = (TextView)v.findViewById(R.id.searchEmptyView);
-		emptyView.setOnTouchListener(new View.OnTouchListener() {
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				return true;
-			}
-		});
-		emptyView.setText("NoPhotos");
-		mView.setAdapter(cameraAdapter = new CameraAdapter(mContext));
-		albumsSorted=GalleryFragment.albumsSorted;
-		if (albumsSorted.isEmpty()) {
-			Toast.makeText(mContext, "No Image Found", Toast.LENGTH_SHORT).show();
-		} else {
-			photos=GalleryFragment.albumsSorted.get(0).photos;
-		}
+    private void initializeView(View v){
+        mView=(GridView)v.findViewById(R.id.grid_view);
+        emptyView = (TextView)v.findViewById(R.id.searchEmptyView);
+        emptyView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+        emptyView.setText("NoPhotos");
+        mView.setAdapter(listAdapter = new ListAdapter(mContext));
 
-		int position = mView.getFirstVisiblePosition();
-		int columnsCount = 2;
-		mView.setNumColumns(columnsCount);
-		itemWidth = (ShowCaseApplication.displaySize.x - ((columnsCount + 1) * ShowCaseApplication.dp(4))) / columnsCount;
-		mView.setColumnWidth(itemWidth);
 
-		cameraAdapter.notifyDataSetChanged();
-		mView.setSelection(position);
-		mView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-				Intent mIntent=new Intent(mContext,PhotoPreviewActivity.class);
-				Bundle mBundle=new Bundle();
-				mBundle.putInt("Key_FolderID", 0);
-				mBundle.putInt("Key_ID", position);
-				mIntent.putExtras(mBundle);
-				startActivity(mIntent);
-			}
-		});
+        mView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+        mView.setMultiChoiceModeListener(new MultiChoiceModeListener());
 
-		if (cameraAdapter != null) {
-			cameraAdapter.notifyDataSetChanged();
-		}
-	}
+        albumsSorted=GalleryFragment.albumsSorted;
+        if (albumsSorted.isEmpty()) {
+            Toast.makeText(mContext, "No Image Found", Toast.LENGTH_SHORT).show();
+        } else {
+            photos=GalleryFragment.albumsSorted.get(0).photos;
+        }
 
-	private class CameraAdapter extends BaseFragmentAdapter {
-		private Context mContext;
-		private LayoutInflater layoutInflater;
-		private DisplayImageOptions options;
-		private ImageLoader imageLoader = ImageLoader.getInstance();
+        int position = mView.getFirstVisiblePosition();
+        int columnsCount = 2;
+        mView.setNumColumns(columnsCount);
+        itemWidth = (ShowCaseApplication.displaySize.x - ((columnsCount + 1) * ShowCaseApplication.dp(4))) / columnsCount;
+        mView.setColumnWidth(itemWidth);
 
-		public CameraAdapter(Context context) {
-			this.mContext = context;
-			this.layoutInflater = (LayoutInflater) mContext
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        listAdapter.notifyDataSetChanged();
+        mView.setSelection(position);
+        /*mView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent mIntent=new Intent(mContext,PhotoPreviewActivity.class);
+                Bundle mBundle=new Bundle();
+                mBundle.putInt("Key_FolderID", 0);
+                mBundle.putInt("Key_ID", position);
+                mIntent.putExtras(mBundle);
+                startActivity(mIntent);
+            }
+        });*/
 
-			options = new DisplayImageOptions.Builder()
-					.showImageOnLoading(R.drawable.nophotos)
-					.showImageForEmptyUri(R.drawable.nophotos)
-					.showImageOnFail(R.drawable.nophotos).cacheInMemory(true)
-					.cacheOnDisc(true).considerExifParams(true).build();
-			imageLoader.init(ImageLoaderConfiguration.createDefault(context));
 
-		}
 
-		@Override
-		public boolean areAllItemsEnabled() {
-			return true;
-		}
+        if (listAdapter != null) {
+            listAdapter.notifyDataSetChanged();
+        }
+    }
 
-		@Override
-		public boolean isEnabled(int i) {
-			return true;
-		}
+    private class ListAdapter extends BaseFragmentAdapter {
+        private Context mContext;
+        private LayoutInflater layoutInflater;
+        private DisplayImageOptions options;
+        private ImageLoader imageLoader = ImageLoader.getInstance();
 
-		@Override
-		public int getCount() {
-			return photos != null ? photos.size() : 0;
-		}
+        public ListAdapter(Context context) {
+            this.mContext = context;
+            this.layoutInflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		@Override
-		public Object getItem(int i) {
-			return null;
-		}
+            options = new DisplayImageOptions.Builder()
+                    .showImageOnLoading(R.drawable.nophotos)
+                    .showImageForEmptyUri(R.drawable.nophotos)
+                    .showImageOnFail(R.drawable.nophotos).cacheInMemory(true)
+                    .cacheOnDisc(true).considerExifParams(true).build();
+            imageLoader.init(ImageLoaderConfiguration.createDefault(context));
 
-		@Override
-		public long getItemId(int i) {
-			return i;
-		}
+        }
 
-		@Override
-		public boolean hasStableIds() {
-			return true;
-		}
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
 
-		@Override
-		public View getView(int i, View view, ViewGroup viewGroup) {
-			viewHolder mHolder;
-			if (view == null) {
-				mHolder = new viewHolder();
-				view = layoutInflater.inflate(R.layout.album_image, viewGroup,false);
-				mHolder.imageView = (ImageView) view.findViewById(R.id.album_image);
-				ViewGroup.LayoutParams params = view.getLayoutParams();
-				params.width = itemWidth;
-				params.height = itemWidth;
-				view.setLayoutParams(params);
-				mHolder.imageView.setTag(i);
+        @Override
+        public boolean isEnabled(int i) {
+            return true;
+        }
 
-				view.setTag(mHolder);
-			} else {
-				mHolder = (viewHolder) view.getTag();
-			}
-			PhoneMediaControl.PhotoEntry mPhotoEntry = photos.get(i);
-			String path = mPhotoEntry.path;
-			if (path != null && !path.equals("")) {
-				ImageLoader.getInstance().displayImage("file://" + path, mHolder.imageView);
-			}
+        @Override
+        public int getCount() {
+            return photos != null ? photos.size() : 0;
+        }
 
-			return view;
-		}
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
 
-		@Override
-		public int getItemViewType(int i) {
-			return 0;
-		}
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
 
-		@Override
-		public int getViewTypeCount() {
-			return 2;
-		}
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
 
-		@Override
-		public boolean isEmpty() {
-			return albumsSorted == null || albumsSorted.isEmpty();
-		}
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            viewHolder mHolder;
+            if (view == null) {
+                mHolder = new viewHolder();
+                view = layoutInflater.inflate(R.layout.album_image, viewGroup,false);
+                mHolder.imageView = (ImageView) view.findViewById(R.id.album_image);
+                ViewGroup.LayoutParams params = view.getLayoutParams();
+                params.width = itemWidth;
+                params.height = itemWidth;
+                view.setLayoutParams(params);
+                mHolder.imageView.setTag(i);
 
-		class viewHolder {
-			public ImageView imageView;
-		}
+                view.setTag(mHolder);
+            } else {
+                mHolder = (viewHolder) view.getTag();
+            }
+            PhoneMediaControl.PhotoEntry mPhotoEntry = photos.get(i);
+            String path = mPhotoEntry.path;
+            if (path != null && !path.equals("")) {
+                ImageLoader.getInstance().displayImage("file://" + path, mHolder.imageView);
+            }
 
-	}
+            return view;
+        }
+
+        @Override
+        public int getItemViewType(int i) {
+            return 0;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return albumsSorted == null || albumsSorted.isEmpty();
+        }
+
+        class viewHolder {
+            public ImageView imageView;
+        }
+
+    }
+
+    public class MultiChoiceModeListener implements GridView.MultiChoiceModeListener{
+
+        @Override
+        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+            PhoneMediaControl.PhotoEntry selectedPhoto = photos.get(position);
+            Toast.makeText(mContext, "selected_photo= "+ selectedPhoto, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+        }
+    }
 }
